ce753eb2fabf9d7890b40d94afcc887af465f0eb
Garima Sharma <42331912+Garima1498@users.noreply.github.com>
2018-11-13 09:47:13 +0000

    Update SignupServlet.java

--- SignupServlet.java
+++ SignupServlet.java
@@ -1,4 +1,5 @@
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -10,86 +11,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author vanshika
- */
-public class SignupServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        String uname = request.getParameter("email");
-            String pswd = request.getParameter("password");
-        try (PrintWriter out = response.getWriter()) {
-             String myurl="jdbc:mysql://localhost:3306/project";
+public class SignupServlet extends HttpServlet {  
+public void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+  
+response.setContentType("text/html");  
+PrintWriter out = response.getWriter();  
+          
+String n=request.getParameter("username");  
+String p=request.getParameter("email");  
+String e=request.getParameter("password");  
+String c=request.getParameter("confirm_password");  
+          
+try{  
+String myurl="jdbc:mysql://localhost:3306/project";
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection conn=DriverManager.getConnection(myurl,"root","");  
-            PreparedStatement stmt = conn.prepareStatement("insert into signup values(?,?)");  
-            
-            stmt.setString(1,uname);  
-            stmt.setString(2,pswd);  
-            stmt.executeUpdate();  
-            System.out.println(" records inserted");  
-            conn.close();  
-            RequestDispatcher req = request.getRequestDispatcher("index.html");
-            req.include(request, response);
-            
-           
-        }
-        catch(Exception e)
-    {
-        System.out.println(e);
-    }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+  
+PreparedStatement ps=conn.prepareStatement(  
+"insert into signup values(?,?,?,?)");  
+  
+ps.setString(1,n);  
+ps.setString(2,p);  
+ps.setString(3,e);  
+ps.setString(4,c);  
+          
+int i=ps.executeUpdate();  
+if(i>0)  
+out.print("You are successfully registered...");  
+      
+          
+}catch (Exception e2) {System.out.println(e2);}  
+          
+out.close();  
+}  
+  
 }
-
