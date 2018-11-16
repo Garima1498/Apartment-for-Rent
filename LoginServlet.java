1bdcb4bd1677704b4b807f3267d8d615fca4b22f
Garima Sharma <42331912+Garima1498@users.noreply.github.com>
2018-11-16 10:32:48 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,53 @@
+
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            /* TODO output your page here. You may use following sample code. */
+
+            String uname = request.getParameter("email");
+            String pswd = request.getParameter("password");
+            //out.println("test");
+            
+            LoginDataBase l = new LoginDataBase();
+        try {
+           int result = l.checkInfo(uname, pswd);
+           if(result > 0)
+           {
+               out.println("login successfull");
+               response.sendRedirect("index_1.html");
+               
+           }
+           else
+           {
+               //out.println("not correct");               response.sendRedirect("index.html");
+           }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+
+        
+    }
+
+
+
+
+}
